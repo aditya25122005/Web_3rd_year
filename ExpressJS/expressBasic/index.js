8033f1a34a5375d75f40f28f9c9e07a0f32b9ad2
aditya25122005 <aditya25pda@gmail.com>
2025-08-02 23:22:47 +0530

    ExpressJS

--- ExpressJS/expressBasic/index.js
+++ ExpressJS/expressBasic/index.js
@@ -1,7 +1,15 @@
 const express=require('express');  // returns a function
 const app=express(); // Returns an Object
-
-// app is an object
+// app is an object(instance of webapp)
 
 //This app is an instance to your entire application
 console.log(app);
+
+// Ask server to listen to your request
+
+// app.listen(8080,()=>{console.log("Server Connected at port 8080");
+// })
+
+
+
+app.listen(8080)
